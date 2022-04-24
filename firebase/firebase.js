c2bd51b0befb732b700bb1a0706b7394a95d7213
Md. Jahidul Islam <97802770+jahidul31@users.noreply.github.com>
2022-04-24 12:29:16 +0600

    Update firebase.js

--- firebase/firebase.js
+++ firebase/firebase.js
@@ -2,12 +2,12 @@ import { initializeApp } from "firebase/app";
 import { getStorage } from "firebase/storage";
 
 const firebaseConfig = {
-  apiKey: "AIzaSyCPqfCAn14RYHnh5CA_Uqcj9oDZXii4Tos",
-  authDomain: "gamerhub-39ec8.firebaseapp.com",
-  projectId: "gamerhub-39ec8",
-  storageBucket: "gamerhub-39ec8.appspot.com",
-  messagingSenderId: "668807926744",
-  appId: "1:668807926744:web:719c61614e4e699ccc58ad",
+  apiKey: YOUR_APP_KEY
+  authDomain: YOUR_DOMAIN
+  projectId: "",
+  storageBucket: "",
+  messagingSenderId: "",
+  appId: "",
 };
 const firebaseApp = initializeApp(firebaseConfig);
 const mystorage = getStorage(firebaseApp);
